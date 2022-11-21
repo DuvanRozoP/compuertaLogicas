efca4a3d1e7230fda600798d04832123e415a413
Duvanrozop <rozoparraduvan@gmail.com>
2022-11-21 01:11:36 -0500

    complete animation
    falta la optimizacion del codigo

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import DomCompuertaLogica from './index.helpers.js';
 
+AOS.init();
+
 const and = new DomCompuertaLogica('.andData1','.andData2','.andDataResult','and1','and2','focoAnd','and');
 const or = new DomCompuertaLogica('.orData1','.orData2','.orDataResult','or1','or2','focoOr','or');
 const xor = new DomCompuertaLogica('.xorData1','.xorData2','.xorDataResult','xor1','xor2','focoxor','xor');
